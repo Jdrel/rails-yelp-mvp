58a8182d985beb185379604b4b2dc129c4be8190
Jascha Drel <jaschadrel@gmail.com>
2018-11-21 15:03:45 +0100

    done, styling is for somebody else :)

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  CATEGORY = ['Chinese', 'Italian', 'Japanese', 'French', 'Belgian']
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: CATEGORY }
+
+  has_many :reviews, dependent: :destroy
+end
